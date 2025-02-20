3a69ed673f4fcb3cf09f822b2d004c5872304e4f
RaqueCR <raquefdez14@gmail.com>
2025-02-20 09:42:10 -0600

    Define estructura Pokemon y vector para almacenamiento

---
+++ proyecto1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Pokemon {
+    string nombre;
+    int id;
+    string tipo;
+    int nivel_poder;
+};
+
+vector<Pokemon> equipoPokemon;
+
+int main() {
+    cout << "Test";
+    return 0;
+}
